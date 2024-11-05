e14d63341f90e79b241cd44d55e0feb31048a482
Mahir29Flame <zarif467396@gmail.com>
2024-11-05 21:25:33 +0500

    Update me.py

--- All of my programs/me.py
+++ All of my programs/me.py
@@ -1,5 +1,5 @@
-a = input("Tumi ke bolo to ?")
-if a == "Abbu":
-    print("Tumi Ekta boka !")
+a = input("Tell who are you ?")
+if a == "Me":
+    print("You're an ediot !!")
 else:
-    print ("Mitha bolo na!")
+    print ("Don't lie")
